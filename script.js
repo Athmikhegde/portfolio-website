48a02cd9b1c106c66c120e035a258bb1e2b97ebd
Athmikhegde <athmik1382001@gmail.com>
2023-07-25 20:01:58 +0530

    contact form working

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ nav.classList.remove('active');
 
 }
 
+//contact form
+function sendEmail(){
+    Email.send({
+Host : "smtp.elasticemail.com",
+Username : "athmikdummy@gmail.com",
+Password : "49725316A75EB2EFC62B6D3F2013BA53B3A9",
+From : "athmikdummy@gmail.com",
+To : 'athmik515@gmail.com',
+Subject : "New Enquiry From portfolio website",
+Body : "Name: "+document.getElementById("name").value
++"<br> Email: "+document.getElementById("email").value
++"<br> Phone No: "+document.getElementById("phone").value
++"<br> Phone No: "+document.getElementById("subject").value
++"<br> Message:  "+document.getElementById("message").value
+}).then(
+message => alert("Message sent, We will contact you back as soon as possible")
+);
+console.log("done");
+  }
+
